9f492c5dd3378f3e0d990d5891837bacb76bf0bc
Selvakumar Murugesan <selvaonline@gmail.com>
2023-04-16 17:33:42 -0500

    adding app.py

---
+++ app.py
@@ -0,0 +1,26 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+stores = [
+    {
+        "name":"myStore",
+        "items" : [
+            {
+                "name" : "chair",
+                "price": 14.99
+            },
+            {
+                "name" :"table",
+                "price":49.99 
+            }
+        ]
+    }
+]
+
+@app.get("/store")
+
+def get_stores():
+    return {
+        "stores": stores
+    }
